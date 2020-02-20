c6dbcc2c7cf588ce7138688c1675084ad32364c5
WarlockDark <ogidog@mail.ru>
2020-02-20 15:52:30 +0700

    update from work

--- src/main/java/org/myapp/utils/ConnectedComponents.java
+++ src/main/java/org/myapp/utils/ConnectedComponents.java
@@ -1,16 +1,19 @@
 package org.myapp.utils;
 
 import com.twitter.chill.java.ArraysAsListSerializer;
+import org.esa.s1tbx.sentinel1.gpf.TOPSARSplitOp;
 import org.esa.snap.core.dataio.ProductIO;
-import org.esa.snap.core.datamodel.Band;
-import org.esa.snap.core.datamodel.Product;
-import org.esa.snap.core.datamodel.ProductData;
+import org.esa.snap.core.datamodel.*;
+import org.esa.snap.core.gpf.OperatorSpi;
+import org.esa.snap.core.gpf.common.SubsetOp;
 
+import java.awt.*;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.List;
 
 public class ConnectedComponents {
 
@@ -38,80 +41,107 @@ public class ConnectedComponents {
                         }
                     }).toArray(Product[]::new);
 
-            int minWidth = products[0].getSceneRasterWidth(), minHeight = products[0].getSceneRasterHeight();
+            int maxWidth = products[0].getSceneRasterWidth(), maxHeight = products[0].getSceneRasterHeight();
             for (Product product : products) {
-                minWidth = product.getSceneRasterWidth() < minWidth ? product.getSceneRasterWidth() : minWidth;
-                minHeight = product.getSceneRasterHeight() < minHeight ? product.getSceneRasterHeight() : minHeight;
+                maxWidth = product.getSceneRasterWidth() > maxWidth ? product.getSceneRasterWidth() : maxWidth;
+                maxHeight = product.getSceneRasterHeight() > maxHeight ? product.getSceneRasterHeight() : maxHeight;
             }
 
-            float[] aveCoh = new float[minWidth * minHeight];
+            float[][] aveCoh = new float[maxHeight][maxWidth];
             for (Product product : products) {
                 product.getBandAt(0).readRasterDataFully();
                 float[] data = ((ProductData.Float) product.getBandAt(0).getData()).getArray();
-                for (int i = 0; i < aveCoh.length; i++) {
-                    aveCoh[i] = aveCoh[i] + data[i];
+                for (int y = 0; y < product.getSceneRasterHeight(); y++) {
+                    for (int x = 0; x < product.getSceneRasterWidth(); x++) {
+                        aveCoh[y][x] = aveCoh[y][x] + data[y * product.getSceneRasterWidth() + x];
+                    }
                 }
                 product.closeIO();
             }
-            for (int i = 0; i < aveCoh.length; i++) {
-                aveCoh[i] = aveCoh[i] / (float) products.length;
+            for (int y = 0; y < maxHeight; y++) {
+                for (int x = 0; x < maxWidth; x++) {
+                    aveCoh[y][x] = aveCoh[y][x] / (float) products.length;
+                }
             }
-            HashMap<Integer, ArrayList> maxCoh = new HashMap<>();
-            for (int i = 0; i < aveCoh.length; i++) {
-                if (aveCoh[i] > minCoh) {
-                    maxCoh.put(i, new ArrayList());
+
+
+            HashMap<Double[], ArrayList> maxCoh = new HashMap<>();
+            for (int y = 0; y < maxHeight; y++) {
+                for (int x = 0; x < maxWidth; x++) {
+                    if (aveCoh[y][x] > minCoh) {
+                        PixelPos pixelPos = new PixelPos(x, y);
+                        GeoPos geoPos = new GeoPos();
+                        products[0].getBandAt(0).getGeoCoding().getGeoPos(pixelPos, geoPos);
+                        maxCoh.put(new Double[]{geoPos.lat, geoPos.lon}, new ArrayList());
+                    }
                 }
             }
 
             Files.walk(Paths.get(workingDir))
                     .filter(file -> file.toAbsolutePath().toString().endsWith("_cc.dim"))
                     .forEach(file -> {
+
                         try {
-                            Product sourceProduct = ProductIO.readProduct(file.toFile());
-                            Band[] bands = sourceProduct.getBands();
+
+                            Product product = ProductIO.readProduct(file.toFile());
+                            Band[] bands = product.getBands();
                             String ccBandName = Arrays.stream(bands).filter(band -> band.getName().contains("Unw_"))
                                     .map(Band::getName).toArray(String[]::new)[0];
-                            sourceProduct.getBand(ccBandName).readRasterDataFully();
-                            float[] data = ((ProductData.Float) sourceProduct.getBand(ccBandName).getData()).getArray();
+
+                            product.getBand(ccBandName).readRasterDataFully();
+                            float[] data = ((ProductData.Float) product.getBand(ccBandName).getData()).getArray();
                             int counter = 0;
                             for (int i = 0; i < data.length; i++) {
                                 if (data[i] > 0.0) {
                                     counter += 1;
                                 }
                             }
-                            sourceProduct.closeIO();
+
                             if (((float) counter / (float) data.length) * 100 > connectedComponentPercent) {
                                 maxCoh.entrySet().stream().forEach(entry -> {
-                                    if (data[entry.getKey()] > 0.0) {
+                                    double lat = entry.getKey()[0];
+                                    double lon = entry.getKey()[1];
+                                    GeoPos geoPos = new GeoPos(lat, lon);
+                                    PixelPos pixelPos = new PixelPos();
+                                    int width = product.getBand(ccBandName).getRasterWidth();
+                                    product.getBand(ccBandName).getGeoCoding().getPixelPos(geoPos, pixelPos);
+                                    if (data[(int) pixelPos.getY() * width + (int) pixelPos.getX()] > 0.0) {
                                         entry.getValue().add(file.getParent());
                                     }
                                 });
 
                                 // TODO: delete
-                                System.out.println(ccBandName + " -> " + counter + " points (" + file.getParent() + ")");
+                                //System.out.println(ccBandName + " -> " + counter + " points (" + file.getParent() + ")");
                                 //
                             }
+
+                            product.closeIO();
+
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
                     });
 
+            Iterator iterator = maxCoh.keySet().iterator();
+            int maxConnectedComponentPathAmount = 0;
+            ArrayList<String> maxConnectedComponentPaths = null;
+            Double[] maxConnectedComponentPoint = null;
+            while (iterator.hasNext()) {
+                Object connectedComponentPoint = iterator.next();
+                ArrayList<String> connectedComponentPaths = maxCoh.get(connectedComponentPoint);
+                if (connectedComponentPaths.size() > maxConnectedComponentPathAmount) {
+                    maxConnectedComponentPathAmount = connectedComponentPaths.size();
+                    maxConnectedComponentPaths = connectedComponentPaths;
+                    maxConnectedComponentPoint = (Double[]) connectedComponentPoint;
+                }
+            }
 
-            int pointWithMaxConnectedComponents = maxCoh.keySet().stream()
-                    .max(new Comparator<Integer>() {
-                        @Override
-                        public int compare(Integer key1, Integer key2) {
-                            return maxCoh.get(key1).size() - maxCoh.get(key2).size();
-                        }
-                    }).get();
+            System.out.println("Y:" + maxConnectedComponentPoint[1] + ", X:" + maxConnectedComponentPoint[0]);
 
-            System.out.println("Y:" + pointWithMaxConnectedComponents / minWidth + ", X:" + (pointWithMaxConnectedComponents - (pointWithMaxConnectedComponents / minWidth) * minWidth));
-
-            List<String> includedPairPaths = maxCoh.get(pointWithMaxConnectedComponents);
-
+            List<String> includedPairPaths = maxConnectedComponentPaths;
             pairPaths.stream().filter(path -> !includedPairPaths.contains(Paths.get(path)))
                     .forEach(path -> {
-                        removeDirectory(new File(path));
+                        //removeDirectory(new File(path));
                     });
 
         } catch (Exception e) {
