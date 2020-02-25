9954e78e597a34a9fc7f604cb9541bc659a1d11a
WarlockDark <ogidog@mail.ru>
2020-02-25 15:04:37 +0700

    update from work

--- src/main/java/org/myapp/satellite/radar/mintpy/Stage0.java
+++ src/main/java/org/myapp/satellite/radar/mintpy/Stage0.java
@@ -48,20 +48,10 @@ public class Stage0 {
 
         switch (proc) {
             case "networkModel1":
-                // 1: by the bPerpCrit, bTempCrit
-                composeIntfPairs(workingDir, fileList, bPerpCrit, bTempCrit);
+                networkModel1Processing(workingDir, fileList);
                 break;
             case "networkModel2":
-                // 2: by the by optimalmaster
-                composeIntfPairs(workingDir, fileList);
-                break;
-            case "networkModel3":
-                // 3: by the bPerpCrit, bTempCrit and two-way direction pairs
-                composeIntfPairsExtended(workingDir, fileList, bPerpCrit, bTempCrit);
-                break;
-            case "networkModel4":
-                // 4: by the optimal master get bperp, btemp, doplerdiff
-                composeIntfPairsByOptimalMaster(workingDir, resultDir, fileList);
+                networkModel2Processing(workingDir, resultDir, fileList);
                 break;
             case "prepFiles":
                 prepFiles(workingDir, resultDir, fileList);
@@ -73,106 +63,7 @@ public class Stage0 {
 
     }
 
-    static void composeIntfPairs(String workingDir, String fileList, String bPerpCrit, String bTempCrit) {
-
-        String pairIDStr = "", pairNameStr = "", pairDateStr = "";
-        TreeSet<String> productNames = new TreeSet<>();
-        String masterProductName, slaveProductName, masterProductDate, slaveProductDate;
-
-        InSARStackOverview.IfgPair[] masterSlavePairs;
-        InSARStackOverview.IfgPair masterSlavePair;
-        InSARStackOverview.IfgStack[] stackOverview;
-
-        Product[] products = Arrays.stream(fileList.split(",")).map(file -> {
-            try {
-                return ProductIO.readProduct(file);
-            } catch (Exception e) {
-                return null;
-            }
-        }).toArray(Product[]::new);
-
-        try {
-
-            stackOverview = InSARStackOverview.calculateInSAROverview(products);
-
-            int counter = 0;
-
-            for (int i = 0; i < stackOverview.length; i++) {
-                masterSlavePairs = stackOverview[i].getMasterSlave();
-                for (int j = i; j < masterSlavePairs.length; j++) {
-                    masterSlavePair = masterSlavePairs[j];
-
-                    double bPerp = masterSlavePair.getPerpendicularBaseline();
-                    double bTemp = masterSlavePair.getTemporalBaseline();
-                    if (masterSlavePair.getMasterMetadata().getAbstractedMetadata().getAttribute("PRODUCT").getData().toString().equals(
-                            masterSlavePair.getSlaveMetadata().getAbstractedMetadata().getAttribute("PRODUCT").getData().toString())) {
-                        continue;
-                    }
-
-                    if (Math.abs(bTemp) <= Integer.valueOf(bTempCrit) && Math.abs(bPerp) <= Integer.valueOf(bPerpCrit)) {
-
-                        Matcher dateMatcher = datePattern.matcher(masterSlavePair.getMasterMetadata().getAbstractedMetadata().getAttribute("PRODUCT").getData().toString());
-                        dateMatcher.find();
-                        masterProductDate = dateMatcher.group();
-
-                        dateMatcher = datePattern.matcher(masterSlavePair.getSlaveMetadata().getAbstractedMetadata().getAttribute("PRODUCT").getData().toString());
-                        dateMatcher.find();
-                        slaveProductDate = dateMatcher.group();
-
-                        pairDateStr = pairDateStr + masterProductDate + "," + slaveProductDate + "," + bPerp + "," + bTemp + ";";
-
-                        masterProductName = masterSlavePair.getMasterMetadata().getAbstractedMetadata().getAttribute("PRODUCT").getData().toString();
-                        slaveProductName = masterSlavePair.getSlaveMetadata().getAbstractedMetadata().getAttribute("PRODUCT").getData().toString();
-                        productNames.add(masterProductName);
-                        productNames.add(slaveProductName);
-
-                        pairNameStr = pairNameStr + masterProductName + "," + slaveProductName + ";";
-
-                        counter++;
-
-                    }
-                }
-            }
-
-            System.out.println("Total pairs: " + counter);
-
-            Arrays.stream(products).forEach(product -> {
-                try {
-                    product.closeIO();
-                } catch (IOException e) {
-                    System.out.println(e);
-                }
-            });
-
-            pairNameStr = pairNameStr.substring(0, pairNameStr.length() - 1);
-            pairIDStr = pairNameStr;
-            String[] productNamesStr = productNames.stream().toArray(String[]::new);
-
-            for (int i = 0; i < productNamesStr.length; i++) {
-                pairIDStr = pairIDStr.replace(productNamesStr[i], String.valueOf(i));
-            }
-
-            new File(workingDir + File.separator + "network").mkdirs();
-
-            // Write pairs to file
-            PrintWriter out = new PrintWriter(workingDir + File.separator + "network" + File.separator + "pairNames.txt");
-            out.println(pairNameStr);
-            out.close();
-
-            out = new PrintWriter(workingDir + File.separator + "network" + File.separator + "pairDate.txt");
-            out.println(pairDateStr);
-            out.close();
-
-            out = new PrintWriter(workingDir + File.separator + "network" + File.separator + "pairID.txt");
-            out.println(pairIDStr);
-            out.close();
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    static void composeIntfPairs(String workingDir, String fileList) {
+    static void networkModel1Processing(String workingDir, String fileList) {
 
         String masterProductName = "";
         String pairNameStr = "";
@@ -218,106 +109,7 @@ public class Stage0 {
         }
     }
 
-    static void composeIntfPairsExtended(String workingDir, String fileList, String bPerpCrit, String bTempCrit) {
-
-        String pairIDStr = "", pairNameStr = "", pairDateStr = "";
-        TreeSet<String> productNames = new TreeSet<>();
-        String masterProductName, slaveProductName, masterProductDate, slaveProductDate;
-
-        InSARStackOverview.IfgPair[] masterSlavePairs;
-        InSARStackOverview.IfgPair masterSlavePair;
-        InSARStackOverview.IfgStack[] stackOverview;
-
-        Product[] products = Arrays.stream(fileList.split(",")).map(file -> {
-            try {
-                return ProductIO.readProduct(file);
-            } catch (Exception e) {
-                return null;
-            }
-        }).toArray(Product[]::new);
-
-        try {
-
-            stackOverview = InSARStackOverview.calculateInSAROverview(products);
-
-            int counter = 0;
-
-            for (int i = 0; i < stackOverview.length; i++) {
-                masterSlavePairs = stackOverview[i].getMasterSlave();
-                for (int j = 0; j < masterSlavePairs.length; j++) {
-                    masterSlavePair = masterSlavePairs[j];
-
-                    double bPerp = masterSlavePair.getPerpendicularBaseline();
-                    double bTemp = masterSlavePair.getTemporalBaseline();
-                    if (masterSlavePair.getMasterMetadata().getAbstractedMetadata().getAttribute("PRODUCT").getData().toString().equals(
-                            masterSlavePair.getSlaveMetadata().getAbstractedMetadata().getAttribute("PRODUCT").getData().toString())) {
-                        continue;
-                    }
-
-                    if (Math.abs(bTemp) <= Integer.valueOf(bTempCrit) && Math.abs(bPerp) <= Integer.valueOf(bPerpCrit)) {
-
-                        Matcher dateMatcher = datePattern.matcher(masterSlavePair.getMasterMetadata().getAbstractedMetadata().getAttribute("PRODUCT").getData().toString());
-                        dateMatcher.find();
-                        masterProductDate = dateMatcher.group();
-
-                        dateMatcher = datePattern.matcher(masterSlavePair.getSlaveMetadata().getAbstractedMetadata().getAttribute("PRODUCT").getData().toString());
-                        dateMatcher.find();
-                        slaveProductDate = dateMatcher.group();
-
-                        pairDateStr = pairDateStr + masterProductDate + "," + slaveProductDate + "," + bPerp + "," + bTemp + ";";
-
-                        masterProductName = masterSlavePair.getMasterMetadata().getAbstractedMetadata().getAttribute("PRODUCT").getData().toString();
-                        slaveProductName = masterSlavePair.getSlaveMetadata().getAbstractedMetadata().getAttribute("PRODUCT").getData().toString();
-                        productNames.add(masterProductName);
-                        productNames.add(slaveProductName);
-
-                        pairNameStr = pairNameStr + masterProductName + "," + slaveProductName + ";";
-
-                        counter++;
-
-                    }
-                }
-            }
-
-            System.out.println("Total pairs: " + counter);
-
-            Arrays.stream(products).forEach(product -> {
-                try {
-                    product.closeIO();
-                } catch (IOException e) {
-                    System.out.println(e);
-                }
-            });
-
-            pairNameStr = pairNameStr.substring(0, pairNameStr.length() - 1);
-            pairIDStr = pairNameStr;
-            String[] productNamesStr = productNames.stream().toArray(String[]::new);
-
-            for (int i = 0; i < productNamesStr.length; i++) {
-                pairIDStr = pairIDStr.replace(productNamesStr[i], String.valueOf(i));
-            }
-
-            new File(workingDir + File.separator + "network").mkdirs();
-
-            // Write pairs to file
-            PrintWriter out = new PrintWriter(workingDir + File.separator + "network" + File.separator + "pairNames.txt");
-            out.println(pairNameStr);
-            out.close();
-
-            out = new PrintWriter(workingDir + File.separator + "network" + File.separator + "pairDate.txt");
-            out.println(pairDateStr);
-            out.close();
-
-            out = new PrintWriter(workingDir + File.separator + "network" + File.separator + "pairID.txt");
-            out.println(pairIDStr);
-            out.close();
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    static void composeIntfPairsByOptimalMaster(String workingDir, String resultDir, String fileList) {
+    static void networkModel2Processing(String workingDir, String resultDir, String fileList) {
 
         try {
             Files.walk(Paths.get(workingDir + File.separator + "network"))
@@ -566,6 +358,7 @@ public class Stage0 {
             // TOPSARSplit
             fileReader = new FileReader(configDir + File.separator + "s1_tops_split.json");
             jsonObject = (JSONObject) parser.parse(fileReader);
+            HashMap jsonParameters = (HashMap) jsonObject.get("parameters");
             jsonParameters1 = (HashMap) jsonObject.get("parameters");
 
             stageParameters.put("TOPSARSplit",
@@ -578,7 +371,7 @@ public class Stage0 {
             // ApplyOrbitFile
             fileReader = new FileReader(configDir + File.separator + "apply_orbit_file.json");
             jsonObject = (JSONObject) parser.parse(fileReader);
-            HashMap jsonParameters = (HashMap) jsonObject.get("parameters");
+            jsonParameters = (HashMap) jsonObject.get("parameters");
 
             HashMap parameters = new HashMap();
             parameters.put("polyDegree", Integer.valueOf(((HashMap) jsonParameters.get("polyDegree")).get("value").toString()));
