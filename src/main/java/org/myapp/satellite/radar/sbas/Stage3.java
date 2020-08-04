5196d08ed486f4c10bd54cafc34bec7b3bddda4f
ogidog <ogidog@mail.ru>
2020-08-04 23:57:11 +0700

    Committed at home

--- src/main/java/org/myapp/satellite/radar/sbas/Stage3.java
+++ src/main/java/org/myapp/satellite/radar/sbas/Stage3.java
@@ -1,11 +1,15 @@
 package org.myapp.satellite.radar.sbas;
 
+import org.esa.snap.core.gpf.graph.Graph;
+import org.esa.snap.core.gpf.graph.GraphIO;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.myapp.utils.ConsoleArgsReader;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -39,7 +43,7 @@ public class Stage3 {
                 files = filesList.split(",");
             }
 
-            String intfDir = outputDir+ File.separator + "intf";
+            String intfDir = outputDir + File.separator + "intf";
             if (Files.exists(Paths.get(intfDir))) {
                 Files.walk(Paths.get(intfDir))
                         .sorted(Comparator.reverseOrder())
@@ -48,11 +52,26 @@ public class Stage3 {
             }
 
             String stage3Dir = outputDir + "" + File.separator + "stage3";
+            String date2NameFile = outputDir + "" + File.separator + "network" + File.separator + "date2Name.txt";
+            String ifgListFile = outputDir + "" + File.separator + "network" + File.separator + "ifg_list.txt";
+
+            HashMap<String,String> date2Name = new HashMap<>();
+            BufferedReader br = new BufferedReader(new FileReader(date2NameFile));
+            for(String line; (line = br.readLine()) != null; ) {
+
+            }
 
             new File(intfDir).mkdirs();
             new File(stage3Dir).mkdirs();
 
-        }catch(Exception e){
+            String graphFile = "filt_intf.xml";
+            FileReader fileReader = new FileReader(graphDir + File.separator + graphFile);
+            Graph graph = GraphIO.read(fileReader);
+            fileReader.close();
+
+            PrintWriter cmdWriter = new PrintWriter(stage3Dir + File.separator + "stage2.cmd", "UTF-8");
+
+        } catch (Exception e) {
             e.printStackTrace();
             return;
         }
@@ -63,7 +82,20 @@ public class Stage3 {
         HashMap<String, HashMap> stageParameters = null;
 
         try {
+            JSONParser parser = new JSONParser();
             stageParameters = new HashMap<>();
+
+            // Subset
+            FileReader fileReader = new FileReader(configDir + File.separator + "subset.json");
+            JSONObject jsonObject = (JSONObject) parser.parse(fileReader);
+            HashMap jsonParameters = (HashMap) jsonObject.get("parameters");
+
+            String geoRegionCoordinates = ((HashMap) jsonParameters.get("geoRegion")).get("value").toString();
+            HashMap parameters = new HashMap();
+            parameters.put("geoRegionCoordinates", geoRegionCoordinates);
+            stageParameters.put("Subset", parameters);
+
+            fileReader.close();
 
         } catch (Exception e) {
             e.printStackTrace();
