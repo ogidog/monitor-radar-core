a038a595576131c97867cfcad7f1115869590c47
ogidog <ogidog@mail.ru>
2020-12-31 01:51:05 +0700

    Committed at home

--- src/main/java/org/myapp/utils/MiscStage.java
+++ src/main/java/org/myapp/utils/MiscStage.java
@@ -3,9 +3,16 @@ package org.myapp.utils;
 import org.esa.snap.core.dataio.ProductIO;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.datamodel.ProductData;
+import org.esa.snap.core.gpf.graph.Graph;
+import org.esa.snap.core.gpf.graph.GraphIO;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.HashMap;
 
 public class MiscStage {
@@ -14,8 +21,8 @@ public class MiscStage {
 
         try {
             HashMap consoleParameters = ConsoleArgsReader.readConsoleArgs(args);
-            //String outputDir = consoleParameters.get("outputDir").toString();
-            //String graphDir = consoleParameters.get("graphDir").toString();
+            String outputDir = consoleParameters.get("outputDir").toString();
+            String graphDir = consoleParameters.get("graphDir").toString();
             String filesList = consoleParameters.get("filesList").toString();
 
             String[] files;
@@ -28,6 +35,37 @@ public class MiscStage {
                 }
 
             }).map(path -> path.toAbsolutePath().toString()).toArray(String[]::new);
+
+            String subsetEsdDir = outputDir + "" + File.separator + "subset_esd";
+            if (Files.exists(Paths.get(subsetEsdDir))) {
+
+                Files.walk(Paths.get(subsetEsdDir))
+                        .sorted(Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .forEach(File::delete);
+            }
+
+            String subsetTopophaseremovalDir = outputDir + "" + File.separator + "subset_topophaseremoval";
+            if (Files.exists(Paths.get(subsetTopophaseremovalDir))) {
+
+                Files.walk(Paths.get(subsetTopophaseremovalDir))
+                        .sorted(Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .forEach(File::delete);
+            }
+
+            String miscStageDir = outputDir + "" + File.separator + "miscstage";
+            if (Files.exists(Paths.get(miscStageDir))) {
+
+                Files.walk(Paths.get(miscStageDir))
+                        .sorted(Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .forEach(File::delete);
+            }
+
+            new File(subsetEsdDir).mkdirs();
+            new File(subsetTopophaseremovalDir).mkdirs();
+            new File(miscStageDir).mkdirs();
 
             Product product = ProductIO.readProduct(files[0]);
             product.getBandAt(0).readRasterDataFully();
@@ -84,6 +122,22 @@ public class MiscStage {
 
             if (subsetX1 == -1) {
                 subsetX1 = width;
+            }
+
+            String graphFile = "Subset.xml";
+            FileReader fileReader = new FileReader(graphDir + File.separator + graphFile);
+            Graph graph = GraphIO.read(fileReader);
+            fileReader.close();
+
+            graph.getNode("Subset").getConfiguration().getChild("region")
+                    .setValue(subsetX0 + "," + subsetY0 + "," + subsetX1 + "," + subsetY1);
+
+            for (int i = 0; i < files.length; i++) {
+                String fileName = Paths.get(files[i]).getFileName().toString().replace(".dim","");
+                FileWriter fileWriter = new FileWriter(miscStageDir + File.separator + fileName + ".xml");
+                GraphIO.write(graph, fileWriter);
+                fileWriter.flush();
+                fileWriter.close();
             }
 
             return;
