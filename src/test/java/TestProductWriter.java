478845a726c5895c26b9cf0e84284b141e07e2db
WarlockDark <ogidog@mail.ru>
2021-03-04 16:25:28 +0700

    update from work

--- src/test/java/TestProductWriter.java
+++ src/test/java/TestProductWriter.java
@@ -3,25 +3,101 @@ import org.esa.snap.core.dataio.ProductIO;
 import org.esa.snap.core.dataio.dimap.DimapProductConstants;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.datamodel.ProductData;
+import org.myapp.utils.ConsoleArgsReader;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Locale;
 
 public class TestProductWriter {
 
     public static void main(String[] args) {
         try {
-            Product product = ProductIO.readProduct("D:\\Temp\\maiskoe2\\snaphuimport\\20200524_VV_20200605_VV.dim");
-            ProductData pd =
-            product.getBandAt(4).get.readRasterData(0,0,1,1,);
-            product.getBandAt(9).readRasterDataFully();
-            product.getBandAt(9).getRasterData().setElemFloatAt(0, Float.NaN);
-            final File file = new File("D:\\Temp\\20200524_VV_20200605_VV.dim");
-            ProductIO.writeProduct(product,
-                    file,
-                    DimapProductConstants.DIMAP_FORMAT_NAME,
-                    false,
-                    ProgressMonitor.NULL);
+            HashMap consoleParameters = ConsoleArgsReader.readConsoleArgs(args);
+            String outputDir = consoleParameters.get("outputDir").toString();
+            // TODO: убрать, записывать в туже папку, переписывать файлы
+            String testOutputDir = "D:\\Temp\\";
+
+            String snaphuimportDir = outputDir + File.separator + "snaphuimport";
+
+            String[] files = Files.walk(Paths.get(snaphuimportDir)).filter(path -> {
+                if (path.toString().endsWith(".dim")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }).map(path -> path.toAbsolutePath().toString()).toArray(String[]::new);
+
+            Product[] products = Arrays.stream(files).map(file -> {
+                try {
+                    Product product = ProductIO.readProduct(file);
+                    return product;
+                } catch (Exception ex) {
+                    return null;
+                }
+            }).toArray(Product[]::new);
+
+            int cohBandIndex = -1, unwBandIndex = -1;
+            for (int i = 0; i < products[0].getBands().length; i++) {
+                if (products[0].getBandAt(i).getName().toLowerCase().contains("coh")) {
+                    cohBandIndex = i;
+                }
+                if (products[0].getBandAt(i).getName().toLowerCase().contains("unw")) {
+                    unwBandIndex = i;
+                }
+            }
+
+            int minWidth = 999999999, minHeight = 999999999;
+            for (int i = 0; i < products.length; i++) {
+                if (products[i].getSceneRasterWidth() < minWidth) {
+                    minWidth = products[i].getSceneRasterWidth();
+                }
+                if (products[i].getSceneRasterHeight() < minHeight) {
+                    minHeight = products[i].getSceneRasterHeight();
+                }
+            }
+
+            boolean[] mask = new boolean[minHeight * minWidth];
+            Arrays.fill(mask, true);
+
+            ProductData pd = ProductData.createInstance(30, minHeight * minWidth);
+            for (int i = 0; i < products.length; i++) {
+                products[i].getBandAt(cohBandIndex).readRasterData(0, 0, minWidth - 1, minHeight - 1, pd);
+                for (int j = 0; j < pd.getNumElems(); j++) {
+                    if (pd.getElemFloatAt(j) > 0.5) {
+                        if (!mask[j]) {
+                            mask[j] = false;
+                        } else {
+                            mask[j] = true;
+                        }
+                    } else {
+                        mask[j] = false;
+                    }
+                }
+            }
+
+            for (int i = 0; i < products.length; i++) {
+                products[i].getBandAt(unwBandIndex).readRasterDataFully();
+                for (int j = 0; j < mask.length; j++) {
+                    if (!mask[j]) {
+                        products[i].getBandAt(unwBandIndex).getRasterData().setElemFloatAt(j, Float.NaN);
+                    }
+                }
+                File file = new File(snaphuimportDir + File.separator + products[i].getName() + ".dim");
+                ProductIO.writeProduct(products[i],
+                        file,
+                        DimapProductConstants.DIMAP_FORMAT_NAME,
+                        false,
+                        ProgressMonitor.NULL);
+                products[i].closeIO();
+
+            }
+
+            return;
 
         } catch (Exception ex) {
             ex.printStackTrace();
