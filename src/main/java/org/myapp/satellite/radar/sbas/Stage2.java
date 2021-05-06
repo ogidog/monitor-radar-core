7d49e1528b44f0026f85a30f0927c3e57cbe1b75
GamerUser <ogidog@mail.ru>
2021-05-06 14:28:13 +0700

    Committed at home

--- src/main/java/org/myapp/satellite/radar/sbas/Stage2.java
+++ src/main/java/org/myapp/satellite/radar/sbas/Stage2.java
@@ -1,6 +1,7 @@
 
 package org.myapp.satellite.radar.sbas;
 
+import org.esa.s1tbx.insar.gpf.InSARStackOverview;
 import org.esa.snap.core.dataio.ProductIO;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.gpf.graph.Graph;
@@ -8,6 +9,7 @@ import org.esa.snap.core.gpf.graph.GraphIO;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.myapp.utils.ConsoleArgsReader;
+import org.myapp.utils.Graph1;
 
 import java.io.File;
 import java.io.FileReader;
@@ -16,11 +18,10 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Stage2 {
@@ -69,7 +70,6 @@ public class Stage2 {
             }
             new File(backgeocodingDir).mkdirs();
 
-
             if (Files.exists(Paths.get(stage2Dir))) {
                 Files.walk(Paths.get(stage2Dir))
                         .sorted(Comparator.reverseOrder())
@@ -80,6 +80,80 @@ public class Stage2 {
 
             ArrayList<String[]> pairs = new ArrayList<>();
             String content = new String(Files.readAllBytes(Paths.get(configDir + File.separator + "selectNetwork.template")));
+
+            if (content.contains("delaunay")) {
+                InSARStackOverview.IfgStack[] stackOverview = InSARStackOverview.calculateInSAROverview(products);
+
+                List<String> configLines = Files.readAllLines(Paths.get(configDir + File.separator + "selectNetwork.template"));
+
+                int bPerpMax = Integer.valueOf(configLines.stream().filter(line -> line.contains("perpBaseMax")).toArray(String[]::new)[0].split("=")[1].trim());
+                int bTempMax = Integer.valueOf(configLines.stream().filter(line -> line.contains("tempBaseMax")).toArray(String[]::new)[0].split("=")[1].trim());
+                int currBPerp = 40, currBTemp = 45;
+
+                Graph1 network = null;
+                boolean visited[] = new boolean[products.length];
+                boolean isFullyConnectedGraph = false;
+
+                int i1, j1 = 0;
+                for (i1 = currBPerp; i1 < bPerpMax; i1++) {
+                    if (isFullyConnectedGraph) {
+                        break;
+                    }
+                    for (j1 = currBTemp; j1 < bTempMax; j1++) {
+                        if (isFullyConnectedGraph) {
+                            break;
+                        }
+                        network = new Graph1(products.length);
+                        for (int i = 0; i < stackOverview.length; i++) {
+                            InSARStackOverview.IfgPair[] masterSlavePairs = stackOverview[i].getMasterSlave();
+                            for (int j = i + 1; j < masterSlavePairs.length; j++) {
+                                if (Math.abs(masterSlavePairs[j].getPerpendicularBaseline()) <= i1 &&
+                                        Math.abs(masterSlavePairs[j].getTemporalBaseline()) <= j1) {
+                                    network.addEdge(i, j);
+                                }
+                            }
+                        }
+
+                        Arrays.fill(visited, false);
+                        visited[0] = true;
+                        visited = network.DFS(0);
+                        isFullyConnectedGraph = true;
+                        for (boolean item : visited) {
+                            if (!item) {
+                                isFullyConnectedGraph = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (isFullyConnectedGraph) {
+                    System.out.println("Fully connected graph with bPerp = " + i1 + ", bTemp = " + j1);
+
+                    String[] productNames = new String[products.length];
+                    for (int i = 0; i < products.length; i++) {
+                        productNames[i] = products[i].getName();
+                    }
+
+                    Iterator<LinkedList<Integer>> masterIter = Arrays.stream(network.adj).iterator();
+                    int k = 0;
+                    while (masterIter.hasNext()) {
+                        String masterName = productNames[k];
+                        Iterator<Integer> slaveIter = masterIter.next().iterator();
+                        while (slaveIter.hasNext()) {
+                            String slaveName = productNames[slaveIter.next()];
+                            pairs.add(new String[]{masterName, slaveName});
+                        }
+                        k += 1;
+                    }
+
+                } else {
+                    System.out.println("Not fully connected graph. You should increase bPerpMax and/or bTempMax parameters");
+                    return;
+                }
+
+            }
+
             if (content.contains("sequential")) {
                 for (int i = 0; i < products.length - 3; i++) {
                     for (int j = i + 1; j < i + 3; j++) {
@@ -89,7 +163,7 @@ public class Stage2 {
             }
 
 
-            TOPSARSplitOpEnv topsarSplitOpEnv = new TOPSARSplitOpEnv();
+            /*TOPSARSplitOpEnv topsarSplitOpEnv = new TOPSARSplitOpEnv();
             String graphFile = "applyorbitfile.xml";
 
 
@@ -122,7 +196,7 @@ public class Stage2 {
             }
 
             cmdWriter.close();
-            topsarSplitOpEnv.Dispose();
+            topsarSplitOpEnv.Dispose();*/
 
         } catch (Exception e) {
             e.printStackTrace();
