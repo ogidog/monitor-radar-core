7b82846aeb7de5823e2dbce3bdccceea45f332b9
WarlockDark <ogidog@mail.ru>
2021-05-12 12:14:24 +0700

    update from work

--- src/main/java/org/myapp/satellite/radar/ds/Stage3.java
+++ src/main/java/org/myapp/satellite/radar/ds/Stage3.java
@@ -11,11 +11,12 @@ import org.myapp.utils.ConsoleArgsReader;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Stage3 {
 
@@ -29,14 +30,28 @@ public class Stage3 {
 
             String esdDir = outputDir + File.separator + "esd";
 
+            String stage3Dir = outputDir + File.separator + "Stage3";
+            if (Files.exists(Paths.get(stage3Dir))) {
+                Files.walk(Paths.get(stage3Dir))
+                        .sorted(Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .forEach(File::delete);
+            }
+            new File(stage3Dir).mkdirs();
+
             HashMap parameters = getParameters(configDir);
             if (parameters == null) {
                 System.out.println("Fail to read parameters.");
                 return;
             }
 
-            String[] subsetRegion = ((HashMap) parameters.get("Subset")).get("geoRegion").toString().split(",");
-
+            List<double[]> subsetRegionGeoCoord = Arrays.stream(((HashMap) parameters.get("Subset")).get("geoRegion").toString().split(","))
+                    .limit(4)
+                    .map(tuple -> {
+                        double lon = Double.valueOf(tuple.split(" ")[0]);
+                        double lat = Double.valueOf(tuple.split(" ")[1]);
+                        return new double[]{lon, lat};
+                    }).collect(Collectors.toList());
 
             String file = Files.walk(Paths.get(esdDir)).filter(path -> {
                 if (path.toString().endsWith(".dim")) {
@@ -50,15 +65,47 @@ public class Stage3 {
             Product product = ProductIO.readProduct(file);
             Band band = product.getBandAt(0);
             band.readRasterDataFully();
-            GeoPos geoPos = new GeoPos(Double.valueOf(subsetRegion[0].split(" ")[1]), Double.valueOf(subsetRegion[0].split(" ")[0]));
-            PixelPos pixelPos = new PixelPos();
-            band.getGeoCoding().getPixelPos(geoPos, pixelPos);
 
+            List<int[]> subsetRegionPixelCoord = subsetRegionGeoCoord.stream()
+                    .map(tuple -> {
+                        GeoPos geoPos = new GeoPos(tuple[1], tuple[0]);
+                        PixelPos pixelPos = new PixelPos();
+                        band.getGeoCoding().getPixelPos(geoPos, pixelPos);
+                        return new int[]{(int) pixelPos.y, (int) pixelPos.x};
+                    }).collect(Collectors.toList());
+
+            band.dispose();
+            product.closeIO();
+            product.dispose();
+
+            int minY = Integer.MAX_VALUE;
+            int minX = Integer.MAX_VALUE;
+            int maxY = Integer.MIN_VALUE;
+            int maxX = Integer.MIN_VALUE;
+
+            for (int i = 0; i < subsetRegionPixelCoord.size(); i++) {
+                if (subsetRegionPixelCoord.get(i)[0] < minY) {
+                    minY = subsetRegionPixelCoord.get(i)[0];
+                }
+                if (subsetRegionPixelCoord.get(i)[1] < minX) {
+                    minX = subsetRegionPixelCoord.get(i)[1];
+                }
+                if (subsetRegionPixelCoord.get(i)[0] > maxY) {
+                    maxY = subsetRegionPixelCoord.get(i)[0];
+                }
+                if (subsetRegionPixelCoord.get(i)[1] > maxX) {
+                    maxX = subsetRegionPixelCoord.get(i)[1];
+                }
+            }
+
+            PrintWriter subsetRegion = new PrintWriter(stage3Dir + File.separator + "subsetRegion.txt", "UTF-8");
+            subsetRegion.print(minY + " " + minX + ";" + maxY + " " + maxX);
+            subsetRegion.close();
 
             return;
 
         } catch (Exception ex) {
-
+            ex.printStackTrace();
         }
     }
 
