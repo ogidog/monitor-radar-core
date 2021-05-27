f41ab3886fbb7ef4c30ec621f155fd6c5d3e8078
GamerUser <ogidog@mail.ru>
2021-05-27 23:14:26 +0700

    Committed at home

--- src/main/java/org/myapp/satellite/radar/NDAI/Stage7.java
+++ src/main/java/org/myapp/satellite/radar/NDAI/Stage7.java
@@ -6,8 +6,11 @@ import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.datamodel.ProductData;
 import org.myapp.utils.ConsoleArgsReader;
 
-import java.io.File;
-import java.util.Arrays;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.HashMap;
 
 public class Stage7 {
@@ -18,24 +21,36 @@ public class Stage7 {
             HashMap consoleParameters = ConsoleArgsReader.readConsoleArgs(args);
             String outputDir = consoleParameters.get("outputDir").toString();
 
-            String stage7Dir = outputDir + "" + File.separator + "stage7";
             String stablePointDir = outputDir + File.separator + "stablepoints";
+            String stablePointIndexesDir = outputDir + File.separator + "stablepointindexes";
+
+            if (Files.exists(Paths.get(stablePointIndexesDir))) {
+                Files.walk(Paths.get(stablePointIndexesDir))
+                        .sorted(Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .forEach(File::delete);
+            }
+            new File(stablePointIndexesDir).mkdirs();
 
             Product stablePointsProduct = ProductIO.readProduct(stablePointDir + File.separator + "stablepoints.dim");
             String[] stablePointsBandNames = stablePointsProduct.getBandNames();
-            String[] yearList = Arrays.stream(stablePointsBandNames).map(bandName -> {
-                return bandName.split("_")[2];
-            }).distinct().toArray(String[]::new);
 
-            for (String bandName: stablePointsBandNames){
+            for (String bandName : stablePointsBandNames) {
                 Band stablePointsBand = stablePointsProduct.getBand(bandName);
                 stablePointsBand.readRasterDataFully();
-                ProductData pd = stablePointsBand.getData();
-
-                return;
+                float[] stablePointFlags = ((ProductData.Float) stablePointsBand.getData()).getArray();
+                ObjectOutputStream ous = new ObjectOutputStream(new FileOutputStream(stablePointIndexesDir + File.separator + bandName + ".dat"));
+                for (int i = 0; i < stablePointFlags.length; i++) {
+                    if (stablePointFlags[i] == 1.0f) {
+                        ous.writeInt(i);
+                    }
+                }
+                ous.flush();
+                ous.close();
             }
 
-            return;
+            stablePointsProduct.closeIO();
+            stablePointsProduct.dispose();
 
         } catch (Exception e) {
             e.printStackTrace();
---
+++ src/main/java/org/myapp/satellite/radar/NDAI/Stage8.java
@@ -0,0 +1,13 @@
+package org.myapp.satellite.radar.NDAI;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class Stage8 {
+
+    public static void main(String[] args) {
+        //ObjectInputStream ois = new ObjectInputStream(new FileInputStream(stablePointIndexesDir + File.separator + bandName + ".dat"));
+        //int[] ia = (int[]) (ois.readObject());
+    }
+}
