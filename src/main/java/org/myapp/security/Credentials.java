4a0a30da10d461859861d976e7e4237bd356485e
WarlockDark <ogidog@mail.ru>
2021-07-27 14:37:45 +0700

    update from work

---
+++ src/main/java/org/myapp/security/Credentials.java
@@ -0,0 +1,10 @@
+package org.myapp.security;
+
+public class Credentials {
+
+    public static String DB_PASSWORD = "rfnfkjucybvrjd";
+    public static String DB_USER = "satimg_adm";
+    public static String DB = "satimgdb";
+    public static String DB_HOST = "10.101.80.252";
+
+}
