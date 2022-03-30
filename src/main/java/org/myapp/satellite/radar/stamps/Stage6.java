3d43a9fecf8af4106865ae44811572e900a88445
WarlockDark <ogidog@mail.ru>
2022-03-30 14:42:59 +0700

    update from work

--- src/main/java/org/myapp/satellite/radar/stamps/Stage6.java
+++ src/main/java/org/myapp/satellite/radar/stamps/Stage6.java
@@ -10,7 +10,7 @@ public class Stage6 {
 
         String operationTaskDir = Common.getOperationTaskDir(tasksDir, username, taskId, Common.OperationName.STAMPS_STAGE3);
         String resultDir = Common.getResultDir(resultsDir, username, taskId);
-        String[] command = {"/opt/stamps_proc/bin/Stage6.sh", operationTaskDir, resultDir};
+        String[] command = {"/opt/stamps-proc/bin/Stage6.sh", operationTaskDir, resultDir};
         Common.runScript(command, operationTaskDir, Common.OperationName.STAMPS_STAGE6);
 
     }
